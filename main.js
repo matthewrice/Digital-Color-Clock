319c72c0f92eef50103cdba853d568908fa599b4
Matthew Rice <mtr.rice@gmail.com>
2016-07-16 21:08:21 -0400

    Completed assignment.

--- main.js
+++ main.js
@@ -1,63 +1,86 @@
 (function(){
+  'use strict';
+
+
   function colorClock(){
-    var clock = new Date(),
-    h = clock.getHours(),
-    m = clock.getMinutes(),
-    s = clock.getSeconds();
+    /*
+     * Date is a built in JavaScript object.  Because it is a constructor, to use
+     * it, create a new instance.
+     */
+    var time = new Date();
+    var clock = document.getElementById('clock');
 
-    if(h.toString().length < 2){
-      h = '0' + h;
-    }
-    if(m.toString().length < 2){
-      m = '0' + m;
-    }
-    if(s.toString().length < 2){
-      s = '0' + s;
-    }
 
-    var time = h + ':' + m + ':' + s;
+    /*
+     * getHours, getMinutes, & getSeconds are built in methods of the Date object
+     * that performs specific functions.
+     */
+    var currentHours = time.getHours();
+    var currentMinutes = time.getMinutes();
+    var currentSeconds = time.getSeconds();
 
-    document.getElementById()
+    /*
+     * Instead of "if statements", use the ternary operator to add 0 to number if
+     * less than 10
+     */
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+    // state am or pm as appropriate.
+    var timeOfDay = (currentHours < 12) ? "am" : "pm";
+    // change the time format from 24 hr to 12 hr format.
+    currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+    // make the hrs component to show 12 rather than 0. This would occur at 12am & 12pm.
+    currentHours = (currentHours === 0) ? 12 : currentHours;
+
+    /*
+     * to display currentHours, currentMinutes, currentSeconds, and timeOfDay as
+     * clock, create a string by concatenating them together.
+     */
+    var currentTime = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+    /*
+     * populate the html span in the DOM by retrieving the child text node by setting
+     * it's nodeValue property. if you prefer, you can accomplish a similar result
+     * by using innerHTML, textContent, or innerText.
+     */
+    document.getElementById("clock").firstChild.nodeValue = currentTime;
+
+    var h = currentHours.toString(16);
+    var m = currentMinutes.toString(16);
+    var s = currentSeconds.toString(16);
+   // console.log(h + m + s);
+    document.body.style.backgroundColor = '#0' + s;
+
+    if (clock.parentElement.querySelector(':hover') === clock) {
+      var x = s[0];
+      var y = s[1];
+
+      clock.textContent = '#00:' + x + x + ':' + y + y;
+    } else {
+      clock.textContent = hour + ":" + minutes + ":" + seconds;
+    }
   }
+
+  // update a percentage of the progress bar as seconds go from 0 to 60.
+  function progressBar(){
+    var progressBarWidth = document.querySelector("#my-progress");
+    var trackTime = new Date();
+    var seconds = trackTime.getSeconds();
+    var counter = 0;
+
+    if (counter === 100){
+      progressBarWidth.style.width;
+    } else {
+      counter = (seconds/60) * 100;
+      progressBarWidth.style.width = counter + "%";
+    }
+  }
+
+  // update the clock every second.
+  setInterval(colorClock, 1000);
+  // update the progress bar every second.
+  setInterval(progressBar, 1000);
+
+
 }());
-
-
-// (function(){
-//   'use strict';
-//
-// function renderTime() {
-//
-//   var currentTime = new Date();
-//   var diem = "AM";
-//   var hour = currentTime.getHours();
-//   var minute = currentTime.getMinutes();
-//   var second = currentTime.getSeconds();
-//
-//   if (hour === 0) {
-//     hour = 12;
-//   }
-//   else if (hour > 12) {
-//     hour = hour - 12;
-//     diem = "PM";
-//   }
-//
-//   if (hour < 10) {
-//     hour = "0" + hour;
-//   }
-//   if (minute < 10) {
-//     minute = "0" + minute;
-//   }
-//   if (second < 10) {
-//     second = "0" + second;
-//   }
-//
-//   var myClock = document.getElementById('clock-display');
-//   myClock.textContent = hour + ":" + minute + ":" + second + " " + diem;
-//   myClock.innerText = hour + ":" + minute + ":" + second + " " + diem;
-//   setTimeOut('renderTime()', 1000);
-//
-// }
-//
-// renderTime();
-//
-// }());
